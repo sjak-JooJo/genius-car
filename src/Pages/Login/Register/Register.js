f72b427c8e58d6ff959657b7cd08b3649b2e964e
JooJo <sjak0101@gmail.com>
2022-09-26 16:58:53 +0100

    added firebase authentication

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,22 +1,35 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import './Register.css';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
 
 const Register = () => {
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+      ] = useCreateUserWithEmailAndPassword(auth);
     const navigate = useNavigate();
 
     const navigateLogin = ()=>{
         navigate('/login');
     }
 
+    if(user){
+        navigate('/home');
+    }
 
     const handleRegister = event =>{
         event.preventDefault();
         const name = event.target.name.value;
-        const email = event.target.name.value;
+        const email = event.target.email.value;
         const password = event.target.password.value;
-       // console.log(event.target.email.value);
-       //console.log(event.target.password.value);
+        //console.log(event.target.email.value);
+        //console.log(event.target.password.value);
+
+        createUserWithEmailAndPassword(email, password);
     }
 
     return (
@@ -29,7 +42,6 @@ const Register = () => {
                 
                 <input type="password" name="password" id="" placeholder="Password"  required/>
                 <input type="submit" value="Register" />
-
             </form>
             <p>Already have an account?  <Link to="/login" className='text-danger pe-auto text-decoration-none' onClick={navigateLogin}>Please Login</Link></p>
         </div>
