177451bee1f9ef04b6bb9b18d93f119e3df2d745
JooJo <sjak0101@gmail.com>
2022-09-27 13:38:27 +0100

    updated email varification

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,28 +1,34 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import './Register.css';
-import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { useCreateUserWithEmailAndPassword, useUpdateProfile } from 'react-firebase-hooks/auth';
 import auth from '../../../firebase.init';
 import SocialLogin from '../SocialLogin/SocialLogin';
+import { useState } from 'react';
+import { async } from '@firebase/util';
 
 const Register = () => {
+    const [agree, setAgree] = useState(false);
     const [
         createUserWithEmailAndPassword,
         user,
         loading,
         error,
-      ] = useCreateUserWithEmailAndPassword(auth);
+    ] = useCreateUserWithEmailAndPassword(auth, { sendEmailVerification: true });
+    const [updateProfile, updating, updateError] = useUpdateProfile(auth);
+
+
     const navigate = useNavigate();
 
-    const navigateLogin = ()=>{
+    const navigateLogin = () => {
         navigate('/login');
     }
 
-    if(user){
-        navigate('/home');
+    if (user) {
+       console.log('user', user);
     }
 
-    const handleRegister = event =>{
+    const handleRegister = async (event) => {
         event.preventDefault();
         const name = event.target.name.value;
         const email = event.target.email.value;
@@ -30,21 +36,34 @@ const Register = () => {
         //console.log(event.target.email.value);
         //console.log(event.target.password.value);
 
-        createUserWithEmailAndPassword(email, password);
+        //const agree = event.target.terms.checked;
+
+        await createUserWithEmailAndPassword(email, password);
+        await updateProfile({ displayName: name });
+        console.log('Updated profile');
+        navigate('/home');
+
     }
 
     return (
         <div className='register-form'>
-            <h2 style={{textAlign: 'center'}}>Please Register</h2>
+            <h2 style={{ textAlign: 'center' }}>Please Register</h2>
             <form onSubmit={handleRegister}>
-                <input type="text" name="name" id="" placeholder="Your Name"/>
-                
-                <input type="email" name="email" id="" placeholder="Email Address"  required />
-                
-                <input type="password" name="password" id="" placeholder="Password"  required/>
-                <input type="checkbox" name="terms" id="terms" />
-                <label htmlFor="terms">Accept Terms and Conditions</label>
-                <input className='w-50 mx-auto btn btn-primary mt-2' type="submit" value="Register" />
+                <input type="text" name="name" id="" placeholder="Your Name" />
+
+                <input type="email" name="email" id="" placeholder="Email Address" required />
+
+                <input type="password" name="password" id="" placeholder="Password" required />
+                <input onClick={() => setAgree(!agree)} type="checkbox" name="terms" id="terms" />
+
+                <label className={`ps-2 ${agree ? '' : 'text-danger'}`} htmlFor="terms">Accept Terms and Conditions</label>
+                {/* <label className={agree ? 'ps-2 text-primary' : 'ps-2 text-danger'} htmlFor="terms">Accept Terms and Conditions</label> */}
+
+                <input
+                    disabled={!agree}
+                    className='w-50 mx-auto btn btn-primary mt-2'
+                    type="submit"
+                    value="Register" />
             </form>
             <p>Already have an account?  <Link to="/login" className='text-danger pe-auto text-decoration-none' onClick={navigateLogin}>Please Login</Link></p>
             <SocialLogin></SocialLogin>
