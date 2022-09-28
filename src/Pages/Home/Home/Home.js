8a710050a019f4d4a548c2f135783dcd9aa80d2a
JooJo <sjak0101@gmail.com>
2022-09-28 22:19:33 +0100

    added dynamic page title using react helmet async

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -2,10 +2,13 @@ import React from 'react';
 import Experts from '../Experts/Experts';
 import Services from '../Services/Services';
 import Banner from '../Banner/Banner';
+import { Helmet } from 'react-helmet-async';
+import PageTitle from '../../Shared/PageTitle/PageTitle';
 
 const Home = () => {
     return (
         <>
+            <PageTitle title="Home"></PageTitle>
             <Banner></Banner>
             <Services></Services>
             <Experts></Experts>
